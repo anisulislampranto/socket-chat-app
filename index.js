fb5a2a93250465b5d01c6400600012ee6d5ec748
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2021-12-25 00:06:55 +0600

    lets create a simple chatting app

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+require('dotenv').config();
+
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
